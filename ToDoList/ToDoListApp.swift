00754935fa3cbe921e9719334e71acae7dedf5e2
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2023-04-13 09:11:02 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Emanuel Gross von Trockau on 2023-04-13.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
